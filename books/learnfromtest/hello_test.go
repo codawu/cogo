17eba43cf4f07847756e58bdbd4676940c285bc5
uwadgnaw <uwadgnaw@gmail.com>
2019-12-27 21:33:56 +0800

    add a Fail test case

--- books/learnfromtest/hello_test.go
+++ books/learnfromtest/hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	const name = "Bob"
-	got := Hello(name)
-	want := "Hello, " + name
-	if got != want {
-		t.Errorf("got '%q' want '%q'", got, want)
-	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		const name = "Bob"
+		got := Hello(name)
+		want := "Hello, " + name
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
+	})
+
+	t.Run("saying hello world when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
+	})
+
 }
