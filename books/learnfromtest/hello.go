29fd5ab338bea435cbd400dc13e829e22ab27bcd
uwadgnaw <uwadgnaw@gmail.com>
2019-12-27 21:49:43 +0800

    support Spanish Hello

--- books/learnfromtest/hello.go
+++ books/learnfromtest/hello.go
@@ -8,6 +8,11 @@ func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
 	}
+
+	if language == "Spanish" {
+		return "Hola, " + name
+	}
+
 	return englishHelloPrefix + name
 }
 
