d69a9d0b920fe3cfa64a8654f726f6d12b2e1404
uwadgnaw <uwadgnaw@gmail.com>
2019-12-28 07:12:39 +0800

    add repeat times

--- books/learnfromtest/iteration/repeat_test.go
+++ books/learnfromtest/iteration/repeat_test.go
@@ -3,8 +3,8 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeat := Repeat("hello")
-	except := "hellohello"
+	repeat := Repeat("hello",5)
+	except := "hellohellohellohellohello"
 
 	if except != repeat {
 		t.Errorf("excepted '%q' but got '%q'", except, repeat)
