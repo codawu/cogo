001dd7ca52aa00d58d00aeafb694bfd40d4eea38
uwadgnaw <uwadgnaw@gmail.com>
2019-12-27 21:38:27 +0800

    add assert func checkout correct

--- books/learnfromtest/hello_test.go
+++ books/learnfromtest/hello_test.go
@@ -3,21 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
+	}
+
 	t.Run("saying hello to people", func(t *testing.T) {
 		const name = "Bob"
 		got := Hello(name)
 		want := "Hello, " + name
-		if got != want {
-			t.Errorf("got '%q' want '%q'", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("saying hello world when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, world"
-		if got != want {
-			t.Errorf("got '%q' want '%q'", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 }
