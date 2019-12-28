6fa5ad5dddddb0ba883018c14c0d94a97d99174a
uwadgnaw <uwadgnaw@gmail.com>
2019-12-28 20:25:07 +0800

    add TestSum

---
+++ books/learnfromtest/arrays/sum_test.go
@@ -0,0 +1,12 @@
+package arrays
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	numbers := [5]int{1, 1, 1, 1, 1}
+	got := Sum(numbers)
+	want := 15
+	if want != got {
+		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	}
+}
