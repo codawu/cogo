31facdf4a71795a40286fe862122f483c47486d3
uwadgnaw <uwadgnaw@gmail.com>
2019-12-28 20:36:55 +0800

    support Sum

--- books/learnfromtest/arrays/sum_test.go
+++ books/learnfromtest/arrays/sum_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestSum(t *testing.T) {
 
 	t.Run("collection of any size", func(t *testing.T) {
-		numbers := [6]int{1, 1, 1, 1, 1,1}
+		numbers := []int{1, 1, 1, 1, 1,1}
 		got := Sum(numbers)
 		want := 6
 		if want != got {
