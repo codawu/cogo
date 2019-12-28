6b643e19d5c608b320fb205f2c54afe0fbdeb862
uwadgnaw <uwadgnaw@gmail.com>
2019-12-28 20:35:32 +0800

    support any size test

--- books/learnfromtest/arrays/sum_test.go
+++ books/learnfromtest/arrays/sum_test.go
@@ -3,10 +3,14 @@ package arrays
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 1, 1, 1, 1}
-	got := Sum(numbers)
-	want := 5
-	if want != got {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := [6]int{1, 1, 1, 1, 1,1}
+		got := Sum(numbers)
+		want := 6
+		if want != got {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
 }
